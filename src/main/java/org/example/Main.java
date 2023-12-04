16c5bd1632f18c9d2275691de6cea7286f43fdc1
RobinA2 <robinagudelo19999@hotmail.com>
2023-12-04 15:34:49 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entrada del procless
+        String nombreUsuario="Jose Alejandro Holguin Mejia";
+        short edadUsuario=19;
+        String telefonoUsuario="3045543422";
+        String documentoIdentidad="1000417437";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        int cuotaDiciembre;
+        String direccionUsuario="cll 138 c Sur 05";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor específico a una variable
+        // desde el teclado (Recibir datos)
+        Scanner leerDato=new Scanner(System.in);
+
+        System.out.println("Hola, soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+
+
+
+
+    }
+}
